e88860c6a455bb57db1c32e13cb7d220336f9eba
Aj <ajgomez5250@gmail.com>
2023-06-06 12:36:52 -0700

    Merge

--- Final.py
+++ Final.py
@@ -66,20 +66,19 @@ def authorized():
     resp = github.authorized_response()
     if resp is None:
         session.clear()
-        flash('Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args))      
+        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
     else:
         try:
             session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
             session['user_data']=github.get('user').data
             #pprint.pprint(vars(github['/email']))
             #pprint.pprint(vars(github['api/2/accounts/profile/']))
-            flash('You were successfully logged in as ' + session['user_data']['login'] + '.')
+            message='You were successfully logged in as ' + session['user_data']['login'] + '.'
         except Exception as inst:
             session.clear()
             print(inst)
-            flash('Unable to login, please try again.')
-    return redirect('/')
-    #Change the render
+            message='Unable to login, please try again.  '
+    return render_template('homeMessage.html', message=message)
     
 @app.route('/googleb4c3aeedcc2dd103.html')
 def render_google_verification():
@@ -116,9 +115,9 @@ def getMenu(menu):
     
 @app.route('/ordered', methods=['GET','POST'])
 def render_ordered():
-    food = "none"
-    drink = "none"
-    dessert = "none"
+    food = []
+    drink = []
+    dessert = []
     filters = {"ID": session['user_data']['id']}
     order = collection1.find_one(filters)
     if order == None:
@@ -131,31 +130,55 @@ def render_ordered():
         doc = {"Food/s":food, "Drink/s":drink, "Dessert/s":dessert, "ID": session['user_data']['id']}
         collection1.insert_one(doc)
     else:
-        newvalues = { '$push': {'Food/s': food}, '$push': {'Drink/s': drink}, '$push': {'Dessert/s': dessert}}
+        if 'Food' in request.form:
+            food=request.form.getlist('Food')
+        if 'Drink' in request.form:
+            drink=request.form.getlist('Drink')
+        if 'Dessert' in request.form:
+            dessert=request.form.getlist('Dessert')
+        newvalues = {'$push': {'Food/s': {'$each': food}, 'Drink/s': {'$each': drink}, 'Dessert/s': {'$each': dessert}}}
         collection1.update_one(filters, newvalues)
     return render_template('ordered.html')
     
 @app.route('/cart')
 def render_cart():
-    order=getOrder()
-    return render_template('cart.html', order=order)
+    if 'github_token' in session:
+        order=getOrder()
+        return render_template('cart.html', order=order)
+    else:
+        return render_template('pleaseLog.html')
     
 def getOrder():
     items=""
-    for food in collection1.find_one()["Food/s"]:
-        f = collection2.find_one({"_id": ObjectId(food)})
-    for drink in collection1.find_one()["Drink/s"]:
-        d = collection2.find_one({"_id": ObjectId(drink)})
-    for dessert in collection1.find_one()["Dessert/s"]:
-        ds = collection2.find_one({"_id": ObjectId(dessert)})
-    items += Markup('<div>' + "Food/s: " + f['Food'] + "<br>" + "Drink/s: " + d['Drink'] + "<br>" + "Dessert/s: " + ds['Dessert'] +"<form action=\"/delete\" method=\"post\"> <button type=\"submit\" name=\"delete\" value=\"" + str(f["Food"]) + str(d["Drink"]) + str(ds["Dessert"]) + "\">Delete</button> </form>" + "</div>")
-    return items
+    filters = {"ID": session['user_data']['id']}
+    menu = collection1.find_one(filters)
+    if menu == None:
+        items="You must first add things to your cart inorder to veiw it."
+    else:
+        if 'Food/s' in menu:
+            for food in menu["Food/s"]:
+                f = collection2.find_one({"_id": ObjectId(food)})
+                items += Markup('<div>' + "Food/s: " + f['Food'] + "<br>" + "<form action=\"/delete\" method=\"post\"> <button type=\"submit\" name=\"Food/s\" value=\"" + str(f["Food"]) + "\">Delete</button> </form>" + "</div>")
+        if 'Drink/s' in menu:            
+            for drink in menu["Drink/s"]:
+                d = collection2.find_one({"_id": ObjectId(drink)})
+                items += Markup('<div>' + "Drink/s: " + d['Drink'] + "<br>" + "<form action=\"/delete\" method=\"post\"> <button type=\"submit\" name=\"Drink/s\" value=\"" + str(d["Drink"]) + "\">Delete</button> </form>" + "</div>")
+        if 'Dessert/s' in menu:
+            for dessert in menu["Dessert/s"]:
+                ds = collection2.find_one({"_id": ObjectId(dessert)})
+                items += Markup('<div>' + "Dessert/s: " + ds['Dessert'] +"<form action=\"/delete\" method=\"post\"> <button type=\"submit\" name=\"Dessert/s\" value=\"" + str(ds["Dessert"]) + "\">Delete</button> </form>" + "</div>")
+        return items
     
 @app.route("/delete", methods=['POST'])
 def renderDelete():
-    if 'delete' in request.form:
-        ID = request.form['delete']
-        collection1.delete_one({'_id': ObjectId(ID)})
+    filters = {"ID": session['user_data']['id']}
+    menu = collection1.find_one(filters)
+    item_type = list(request.form.keys())[0][:-2]
+    item_filter = {item_type: list(request.form.values())[0]}
+    dis = collection2.find_one(item_filter)
+    var = list(request.form.keys())[0]
+    deletevalues = {'$pull': {var: str(dis['_id'])}}
+    collection1.update_one(filters, deletevalues)
     return redirect(url_for("render_cart"))
 
 if __name__ == '__main__':
